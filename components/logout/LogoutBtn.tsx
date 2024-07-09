d95c642ea67f2c79b12db6ba26dc820308bab67c
Roshan-Roy <roshanroy2911@gmail.com>
2024-07-09 23:37:39 +0530

    first version

--- components/logout/LogoutBtn.tsx
+++ components/logout/LogoutBtn.tsx
@@ -3,14 +3,19 @@
 import { signOut } from "next-auth/react"
 import styles from "./logout.module.css"
 import { MdLogout } from "react-icons/md"
+import { useState } from "react"
 
 const LogoutBtn = () => {
+    const [clicked, setClicked] = useState(false)
     const handleClick = () => {
-        signOut()
+        if (!clicked) {
+            setClicked(true)
+            signOut()
+        }
     }
     return (
-        <div className={styles.logout_btn} onClick={handleClick}>
-            <MdLogout className={styles.icon}/>
+        <div className={clicked ? `${styles.logout_btn} ${styles.disabled}` : styles.logout_btn} onClick={handleClick}>
+            <MdLogout className={styles.icon} />
             <p>Logout</p>
         </div>
     )
